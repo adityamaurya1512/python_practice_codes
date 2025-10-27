9a6fabb00c51ff0231a800fbbf99c5cf87627ed8
Aditya Maurya <adityamaurya@Adityas-MacBook-Air.local>
2025-10-27 20:33:16 +0530

    list_comprehension more

--- list_comprehension.py
+++ list_comprehension.py
@@ -3,3 +3,16 @@ nums=[1,4,8]
 new_list=[n+1 for n in nums]
 
 print(new_list)
+
+
+name = "Aditya"
+
+char_list = [letter for letter in name]
+
+print(char_list)
+
+
+list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
+numbers = [int(string_) for string_ in list_of_strings]
+result = [n for n in numbers if n%2==0]
+print(result)
