e291218c93a45566dc1a012e61768f3cc77319e1
Aditya Maurya <adityamaurya@Adityas-MacBook-Air.local>
2025-10-28 11:58:08 +0530

    tuples

---
+++ tuple_.py
@@ -0,0 +1,14 @@
+
+
+thistuple=(1,2,"aditya")
+print(type(thistuple))
+
+
+thisisnottuple=(1)   #   thisisnottuple=(1,)  this will be a tuple
+print(type(thisisnottuple))
+
+
+print(thistuple[0])
+
+#thistuple[0]=3     , tuples are immutable , cant assign value to it 
+
